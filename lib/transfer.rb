2a6c550d80b32b6d5e4ef8bbfeeaa8be609bd7ca
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 20:24:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,8 @@ class Transfer
       @receiver.deposit(@amount)
       @status = "complete"
     else
-      "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
   def reverse_transfer
