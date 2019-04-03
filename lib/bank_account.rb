da5a77a17c5836266d9f8e2e85cb871ce563cc3c
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 20:03:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -4,8 +4,17 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
-  attr_reader :name, :balance
+  attr_reader :name, :status
   def deposit(value_being_deposited)
     @balance += value_being_deposited
   end
+  def display_balance
+    @balance
+  end
+  def valid?
+    if self.status == "open" && display_balance > 0
+      true
+    else
+      false
+    end
 end
