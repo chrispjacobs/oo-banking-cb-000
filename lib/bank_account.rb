093c0ca5215a25cb470b61f7c8c5afa631ffe18e
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 20:01:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,6 +2,10 @@ class BankAccount
   def initialize(name)
     @name = name
     @balance = 1000
+    @status = "open"
   end
   attr_reader :name, :balance
+  def deposit(value_being_deposited)
+    @balance += value_being_deposited
+  end
 end
