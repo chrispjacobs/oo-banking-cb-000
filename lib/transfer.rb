eb87cef07967eb39340c8e39fe7de27858681e77
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 20:14:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,4 +4,20 @@ class Transfer
     @receiver = receiver
     @transfer_amount = transfer_amount
     @status = "pending"
+    @completed = false
+  end
+  def valid?
+    if @sender.valid? == true && @receiver.valid? == true
+      true
+    else
+      false
+    end
+  end
+  def execute_transaction
+    if @completed == false && self.valid? == true
+      @sender.deposit(-@transfer_amount)
+      @receiver.deposit(@transfer_amount)
+      @completed = true
+    end
+  end
 end
